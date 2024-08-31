6babf24d5b4252f2d9e1ff643ca8a32a4035c56e
v.pecheritsya <pecheritsa.vv@gmail.com>
2024-08-31 13:14:01 +0300

    Feature: install vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-const app = createApp(App)
+import App from './App.vue';
+import router from './router';
 
-app.use(createPinia())
-app.use(router)
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(vuetify);
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
